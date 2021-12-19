ec3977a104d469d9c88da632a2841f5d6e79e71b
5ham5h33r <Shamsheerkhalid99@gmail.com>
2021-12-19 09:33:59 +0530

    Update Armstrong_Number.java

--- Armstrong_Number.java
+++ Armstrong_Number.java
@@ -1,24 +1,25 @@
+/* Program to check if entered number is an Armstrong Number 
+   Armstrong number is a number that is equal to the sum of cubes of its digits */
 package com.myenterprise;
 
 import java.util.Scanner;
 public class Armstrong_Number {
 
     public static void main(String[] args) {
-        // Armstrong number
         int cube = 0, og, d;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number");
         int n = sc.nextInt();
-        og = n;
+        og = n; //Stores a copy of original number
         while(n!=0)
         {
-            d=n%10;
-            cube=cube+(d*d*d);
+            d=n%10; //Extracts last digit from the right
+            cube += (d*d*d); //Cubes the extracted digit and adds it to cube
             //System.out.println(cube);
-            n=n/10;
+            n=n/10; //Reduces the number of digits in n by 1 (as it's an integer)
         }
         System.out.println("Sum of cube of digits is "+cube);
-        if (cube==og)
+        if (cube==og) //Check if sum of cubes of digit is equal to original number
         System.out.println("Therefore, it is an armstrong number.");
         else
         System.out.println("Therefore, it is not an armstrong number:");
