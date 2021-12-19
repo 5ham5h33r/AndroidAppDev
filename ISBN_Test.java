8ace1d59d524f2403ecc0b0f705360f950dc6f66
5ham5h33r <Shamsheerkhalid99@gmail.com>
2021-12-19 09:45:53 +0530

    Update ISBN_Test.java

--- ISBN_Test.java
+++ ISBN_Test.java
@@ -1,28 +1,33 @@
+/* 
+The ISBN is a ten-digit unique number. With the help of the ISBN, we can easily find any book.
+The ISBN number is a legal number when 1*Digit1 + 2*Digit2 + 3*Digit3 + 4*Digit4 + 5*Digit5 + 6*Digit6 + 7*Digit7 + 8*Digit8 + 9*Digit9 + 10*Digit10 is divisible by 11
+*/
 package com.myenterprise;
 
 import java.util.Scanner;
-import java.lang.*;
+import java.lang.*; //Importing Integer to convert String to non-ASCII int
 public class ISBN_Test {
     public static void main (String[] args){
-        String str;
+        String str; //Initial String declaration for user input
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter ISBN:");
-        str=sc.next();
-        int len=str.length(),sum=0, count = 10;
-        if (len!=10)
+        str=sc.next(); //Taking String input from user
+        int len=str.length(); //Declaring and initializing length of str to len
+        int sum=0, count = 10; //declaring a variable sum to check legality of ISBN and a variable count as counter variable
+        if (len!=10) //Check if length of input String is 10.
         {
             System.out.println("Invalid ISBN");
         }
         else
         {
-            for(int i=0; i<len; i++)
+            for(int i=0; i<len; i++) //Running a loop through the length of the String
             {
                 //System.out.println("Check char at i is "+(Integer.parseInt(str.substring(i,i+1))));
-                sum = (Integer.parseInt(str.substring(i,i+1)) * count) + sum;
+                sum += (Integer.parseInt(str.substring(i,i+1)) * count); //Extracting the first index of the string, converting it to integer and mupltiplying it by counter
                 //System.out.println(sum);
-                count--;
+                count--; //Decrementing counter
             }
-            if (sum%11 == 0)
+            if (sum%11 == 0) //Checking if divisible by 11 for legality of ISBN
             {
                 System.out.println("Valid ISBN");
             }
